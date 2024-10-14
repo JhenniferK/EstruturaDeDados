ced5221b105b32bc87dd26c72592503c9850dcae
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2024-10-14 17:14:35 -0300

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,30 @@
+//Ordenar elementos da lista
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula1 {
+    public static void main(String[] args) {
+        List<Integer> valores = new ArrayList<>();
+        valores.add(25);
+        valores.add(13);
+        valores.add(3);
+        valores.add(17);
+
+        List<Integer> ordenada = new ArrayList<>();
+
+        while (!valores.isEmpty()) {
+            int menor = valores.get(0);
+            for (int i = 1; i < valores.size(); i++) {
+                if (valores.get(i) < menor) {
+                    menor = valores.get(i);
+                }
+            }
+
+            ordenada.add(menor);
+            valores.remove(Integer.valueOf(menor));
+        }
+
+        System.out.println(ordenada);
+    }
+}
